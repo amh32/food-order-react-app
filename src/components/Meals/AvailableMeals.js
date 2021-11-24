8157981a629115faf51bf432700825f0ba9af452
Alex Hastie <hastiea32@gmail.com>
2021-11-24 17:59:32 -0500

    added fetch for meals

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,21 +1,48 @@
 import "./AvailableMeals.css"
-import React from "react";
-import DUMMY_MEALS from "./dummy-meals.js"
+import React, { useState, useEffect } from "react";
 import MealItem from "./MealItem";
 import Card from "../../UI/Card/Card";
 
 const AvailableMeals = props => {
-    const meals = DUMMY_MEALS.map(meal => {
-        return <li key={meal.id}>{<MealItem
+    const [meals, setMeals] = useState([]);
+
+    // const [loading, setLoading] = useState(false);
+
+    // const [error, setError] = useState(null);
+    // function errorHandler() {
+    //     setError("something went wrong");
+    //     throw new Error("something went wrong");
+    // }
+
+    async function fetchArray(url) {
+        return fetch(url).then((response) => {
+            return response.json();
+        }).then((data) => {
+            const array = [];
+            for (const key in data) array.push(data[key]);
+            return array;
+        });
+    }
+
+    async function fetchMeals() {
+        const url = "https://react-http-82519-default-rtdb.firebaseio.com/meals.json";
+        const tempmeals = await fetchArray(url);
+        setMeals(tempmeals);
+    }
+
+    useEffect(fetchMeals);
+    
+    const mealList = meals.map(meal => {
+        return <li key={meal.id}><MealItem
             meal={meal}
             id={meal.id}
             name={meal.name}
             description={meal.description}
             price={meal.price}
-        />}</li>
+        /></li>
     });
     return <Card className='meals'>
-        <ul>{meals}</ul>
+        <ul>{mealList}</ul>
     </Card>
 }
 

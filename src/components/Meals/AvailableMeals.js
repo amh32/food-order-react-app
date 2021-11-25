1db2eed97e4ca3d7f3f167d82884a5c808afe25d
Alex Hastie <hastiea32@gmail.com>
2021-11-25 17:28:37 -0500

    added loading and error messages

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -6,32 +6,53 @@ import Card from "../../UI/Card/Card";
 const AvailableMeals = props => {
     const [meals, setMeals] = useState([]);
 
-    // const [loading, setLoading] = useState(false);
+    const [loading, setLoading] = useState(false);
 
-    // const [error, setError] = useState(null);
-    // function errorHandler() {
-    //     setError("something went wrong");
-    //     throw new Error("something went wrong");
-    // }
+    const [error, setError] = useState(null);
+    function errorHandler() {
+        // setError("something went wrong");
+        throw new Error("something went wrong");
+    }
 
-    async function fetchArray(url) {
-        return fetch(url).then((response) => {
-            return response.json();
-        }).then((data) => {
-            const array = [];
-            for (const key in data) array.push(data[key]);
-            return array;
+
+
+    useEffect(() => {
+        setLoading(true);
+
+        async function fetchArray(url) {
+            return fetch(url).then((response) => {
+                return response.json();
+            }).catch(errorHandler).then((data) => {
+                const array = [];
+                for (const key in data) array.push(data[key]);
+                return array;
+            });
+        }
+
+        async function fetchMeals() {
+            const url = "https://react-http-82519-default-rtdb.firebaseio.com/meals.json";
+            const tempmeals = await fetchArray(url);
+            setMeals(tempmeals);
+            return tempmeals;
+        }
+        
+        fetchMeals().catch(error => {
+            setLoading(false);
+            setError(error.message);
         });
-    }
 
-    async function fetchMeals() {
-        const url = "https://react-http-82519-default-rtdb.firebaseio.com/meals.json";
-        const tempmeals = await fetchArray(url);
-        setMeals(tempmeals);
-    }
+        setLoading(false);
+        return clearTimeout();
+    }, []);
 
-    useEffect(fetchMeals);
-    
+    if (loading) return <section className='loading'>
+        <p>Loading</p>
+    </section>
+
+    if (error) return <section className='error'>
+        <p>{error}</p>
+    </section>
+
     const mealList = meals.map(meal => {
         return <li key={meal.id}><MealItem
             meal={meal}
